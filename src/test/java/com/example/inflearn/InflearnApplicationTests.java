afe38b4edba5e856692a0cea66e04a7f35f31da6
YOONGUNYONG <backendgun@gmail.com>
2024-07-06 19:13:55 +0900

    initPush

---
+++ src/test/java/com/example/inflearn/InflearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inflearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
